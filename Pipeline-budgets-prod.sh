a032682e413f912f588881ed146a4a30d503be8d
Fernando Blat <ferblape@gmail.com>
2018-07-27 09:05:31 +0200

    Fix organizaiton id

--- Pipeline-budgets-prod.sh
+++ Pipeline-budgets-prod.sh
@@ -2,6 +2,9 @@
 
 DEV=/var/www/
 
+# Set organization_id
+echo "diba" > /tmp/diba/organization_id.txt
+
 # Extract > Download data sources
 cd $DEV/gobierto-etl-utils/current/; ruby operations/download/run.rb "https://do.diba.cat/api/dataset/ingressos/token/98e0ab9462ee6b20427441dbab8426b9/pag-fi/999999/camp-instantania/2015-12" /tmp/diba/budgets-2015-income.json
 cd $DEV/gobierto-etl-utils/current/; ruby operations/download/run.rb "https://do.diba.cat/api/dataset/ingressos/token/98e0ab9462ee6b20427441dbab8426b9/pag-fi/999999/camp-instantania/2016-12" /tmp/diba/budgets-2016-income.json
@@ -23,7 +26,7 @@ cd $DEV/gobierto-etl-utils/current/; ruby operations/check-json/run.rb /tmp/diba
 cd $DEV/gobierto-etl-utils/current/; ruby operations/check-json/run.rb /tmp/diba/budgets-2018-expenses.json
 
 # Load > Remove previous data
-cd $DEV/gobierto-etl-diba/current/; ruby operations/gobierto_budgets/clear-budgets/run.rb diba
+cd $DEV/gobierto-etl-utils/current/; ruby operations/gobierto_budgets/clear-budgets/run.rb /tmp/diba/organization_id.txt
 
 # Load > Import income data
 cd $DEV/gobierto/current/; bin/rails runner $DEV/gobierto-etl-diba/current/operations/gobierto_budgets/import-budgets/run.rb /tmp/diba/budgets-2015-income.json 2015
@@ -38,7 +41,6 @@ cd $DEV/gobierto/current/; bin/rails runner $DEV/gobierto-etl-diba/current/opera
 cd $DEV/gobierto/current/; bin/rails runner $DEV/gobierto-etl-diba/current/operations/gobierto_budgets/import-budgets/run.rb /tmp/diba/budgets-2018-expenses.json 2018
 
 # Load > Calculate totals
-echo "diba" > /tmp/diba/organization_id.txt
 cd $DEV/gobierto-etl-utils/current/; ruby operations/gobierto_budgets/update_total_budget/run.rb "2015 2016 2017 2018" /tmp/diba/organization_id.txt
 
 # Load > Calculate bubbles
